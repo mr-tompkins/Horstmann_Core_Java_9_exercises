82fe1a294848b2a161298b038f5cd437bd867c07
mr-tompkins <mr.tompkins@void.com>
2020-04-16 12:49:06 +0200

    Reformat code, make constants private, correct typos in CH8 Ex10

--- src/test/java/chapter8/exercise10/AvgStringLengthCalculatorTest.java
+++ src/test/java/chapter8/exercise10/AvgStringLengthCalculatorTest.java
@@ -13,18 +13,19 @@ import static org.junit.Assert.assertThat;
 @RunWith(JUnit4.class)
 public class AvgStringLengthCalculatorTest {
 
-    final Stream<String> NOT_EMPTY_STREAM = Stream.of("a", "bb", "ccc", "dddd");
-    final double NOT_EMPTY_STREAM_AVG_WORD_LEN = 2.5;
-    final Stream<String> EMPTY_STREAM = Stream.empty();
-    final double EMPTY_STREAM_AVG_WORD_LEN = 0;
+    private final Stream<String> NOT_EMPTY_STREAM = Stream.of("a", "bb", "ccc", "dddd");
+    private final double EXPECTED_NOT_EMPTY_STREAM_AVG_WORD_LEN = 2.5;
+
+    private final Stream<String> EMPTY_STREAM = Stream.empty();
+    private final double EXPECTED_EMPTY_STREAM_AVG_WORD_LEN = 0;
 
     @Test
     public void whenStreamNotEmpty_returnsAverageWorldLength() {
-        assertThat(calculate(NOT_EMPTY_STREAM), equalTo(NOT_EMPTY_STREAM_AVG_WORD_LEN));
+        assertThat(calculate(NOT_EMPTY_STREAM), equalTo(EXPECTED_NOT_EMPTY_STREAM_AVG_WORD_LEN));
     }
 
     @Test
-    public void whenStreamEmopty_returnsZero() {
-        assertThat(calculate(EMPTY_STREAM), equalTo(EMPTY_STREAM_AVG_WORD_LEN));
+    public void whenStreamEmpty_returnsZero() {
+        assertThat(calculate(EMPTY_STREAM), equalTo(EXPECTED_EMPTY_STREAM_AVG_WORD_LEN));
     }
 }
