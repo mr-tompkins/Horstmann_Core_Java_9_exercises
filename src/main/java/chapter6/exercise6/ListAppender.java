52501369e7c234cd646ba994465de40aa91b9d0e
mr-tompkins <mr.tompkins@void.com>
2020-04-26 15:02:16 +0200

    Add null checks for methods parameters in CH6 Ex6

--- src/main/java/chapter6/exercise6/ListAppender.java
+++ src/main/java/chapter6/exercise6/ListAppender.java
@@ -5,12 +5,18 @@ import java.util.List;
 public final class ListAppender {
 
     public static <T> void append1(final List<? extends T> source, final List<T> destination) {
+        if (source == null || destination == null) {
+            throw new IllegalArgumentException();
+        }
         for (T e : source) {
             destination.add(e);
         }
     }
 
     public static <T> void append2(final List<T> source, final List<? super T> destination) {
+        if (source == null || destination == null) {
+            throw new IllegalArgumentException();
+        }
         for (T e : source) {
             destination.add(e);
         }
